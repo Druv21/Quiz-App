15508262d2a63f29c625d21e048fdcd7c710bee2
Druv21 <dhruvag911@gmail.com>
2022-01-13 03:11:25 +0530

    Updated instructions

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -8,11 +8,12 @@ const Instructions = () => {
         </h2>
         <div className="info">
             1. Attempt all the questions.<br/>
-            2. You have 100 seconds to attempt all the questions.<br/>
-            3. You will be rewarded 1 mark for every correct answer and 0 mark for eery wrong answer.<br/>
-            4. Click on Previous button for viewing the previous question.<br/>
-            5. Click on Next button for viewing the next question.<br/>
-            6. Click on Play Again button to attempt the quiz again.
+            2. There are 5 questions in the quiz.<br/>
+            3. You have 100 seconds to attempt all the questions.<br/>
+            4. You will be rewarded 1 mark for every correct answer and 0 mark for eery wrong answer.<br/>
+            5. Click on Previous button for viewing the previous question.<br/>
+            6. Click on Next button for viewing the next question.<br/>
+            7. Click on Play Again button to attempt the quiz again.
         </div>
         </>
     )
